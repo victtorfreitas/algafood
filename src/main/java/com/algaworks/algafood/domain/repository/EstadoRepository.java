ab5337ef106d7959f0b3f98e425cbb48fb2eec19
Victtor <victtorfreitas95@gmail.com>
2020-12-30 15:44:15 -0300

    Adiciona novo model Estado

---
+++ src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.domain.repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Estado;
+
+import java.util.List;
+
+public interface EstadoRepository {
+
+    List<Estado> todos();
+
+    Estado porId(Long id);
+
+    Estado adicionar(Estado estado);
+
+    void remover(Estado estado);
+}
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+
+import java.util.List;
+
+public class EstadoRepositoryImpl implements EstadoRepository {
+
+    @Override
+    public List<Estado> todos() {
+        return null;
+    }
+
+    @Override
+    public Estado porId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Estado adicionar(Estado estado) {
+        return null;
+    }
+
+    @Override
+    public void remover(Estado estado) {
+
+    }
+}
