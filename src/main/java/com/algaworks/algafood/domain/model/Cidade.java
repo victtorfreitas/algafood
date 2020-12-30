d19ecb7091d0b31eb8ca5ac0a708a8980e3e6d21
Victtor <victtorfreitas95@gmail.com>
2020-12-30 15:43:32 -0300

    Adiciona novo model Cidade

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Estado estado;
+}
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CidadeRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+
+import java.util.List;
+
+public class CidadeRepositoryImpl implements CidadeRepository {
+
+    @Override
+    public List<Cidade> todas() {
+        return null;
+    }
+
+    @Override
+    public Cidade porId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Cidade adicionar(Cidade cidade) {
+        return null;
+    }
+
+    @Override
+    public void remover(Cidade cidade) {
+
+    }
+}
