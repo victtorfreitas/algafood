56f48211c3634722f98b29af835d3ecb91e14b18
Victtor <victtorfreitas95@gmail.com>
2020-12-30 15:43:56 -0300

    Atualiza model Cozinha

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,15 +1,24 @@
 package com.algaworks.algafood.domain.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cozinha {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
 }
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+import java.util.List;
+
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+    @Override
+    public List<Cozinha> todas() {
+        return null;
+    }
+
+    @Override
+    public Cozinha porId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Cozinha adicionar(Cozinha cozinha) {
+        return null;
+    }
+
+    @Override
+    public void remover(Cozinha cozinha) {
+
+    }
+}
