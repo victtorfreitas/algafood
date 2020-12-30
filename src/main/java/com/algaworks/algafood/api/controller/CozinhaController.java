a2d9861d0a6a410835eddf5b2b86ca3b6520fc6d
Victtor <victtorfreitas95@gmail.com>
2020-12-30 17:06:40 -0300

    Adiciona controller Cozinha

---
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    private final CozinhaRepository cozinhaRepository;
+
+    public CozinhaController(CozinhaRepository cozinhaRepository) {
+        this.cozinhaRepository = cozinhaRepository;
+    }
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.todas();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
+        return cozinhaRepository.porId(id);
+    }
+}
--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -2,19 +2,27 @@ package com.algaworks.algafood.infrastructure.repository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Component
 public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
 
     @Override
     public List<Cozinha> todas() {
-        return null;
+        return manager.createQuery("from Cozinha", Cozinha.class)
+                .getResultList();
     }
 
     @Override
     public Cozinha porId(Long id) {
-        return null;
+        return manager.find(Cozinha.class, id);
     }
 
     @Override
