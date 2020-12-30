7c5afcf72279bdd59ffe6f1765cc2910741233b8
Victtor <victtorfreitas95@gmail.com>
2020-12-30 17:43:22 -0300

    Atualiza resposta buscar cozinha

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,11 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
-        return cozinhaRepository.porId(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id){
+        Cozinha cozinha = cozinhaRepository.porId(id);
+        if(cozinha != null){
+            return ResponseEntity.ok(cozinha);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
