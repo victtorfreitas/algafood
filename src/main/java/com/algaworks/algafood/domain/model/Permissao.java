2b120815056911783300018b760e2427d99f9fde
Victtor <victtorfreitas95@gmail.com>
2020-12-30 15:45:03 -0300

    Adiciona novo model Permissao

---
+++ src/main/java/com/algaworks/algafood/domain/model/Permissao.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Permissao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+}
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/PermissaoRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.Permissao;
+import com.algaworks.algafood.domain.repository.PermissaoRepository;
+
+import java.util.List;
+
+public class PermissaoRepositoryImpl implements PermissaoRepository {
+
+    @Override
+    public List<Permissao> todas() {
+        return null;
+    }
+
+    @Override
+    public Permissao porId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Permissao adicionar(Permissao permissao) {
+        return null;
+    }
+
+    @Override
+    public void remover(Permissao permissao) {
+
+    }
+}
