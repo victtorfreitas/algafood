8cdba76bec9f1f0c70e57f2c605c672b75a09e25
Victtor <victtorfreitas95@gmail.com>
2020-12-30 15:44:46 -0300

    Adiciona novo model Forma pagamento

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/FormaPagamentoRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.FormaPagamento;
+import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
+
+import java.util.List;
+
+public class FormaPagamentoRepositoryImpl implements FormaPagamentoRepository {
+
+    @Override
+    public List<FormaPagamento> todas() {
+        return null;
+    }
+
+    @Override
+    public FormaPagamento porId(Long id) {
+        return null;
+    }
+
+    @Override
+    public FormaPagamento adicionar(FormaPagamento formaPagamento) {
+        return null;
+    }
+
+    @Override
+    public void remover(FormaPagamento formaPagamento) {
+
+    }
+}
