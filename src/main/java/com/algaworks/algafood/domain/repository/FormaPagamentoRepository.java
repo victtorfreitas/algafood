80ac241975956e53e4582ebe9587f3bef71188b3
Victtor <victtorfreitas95@gmail.com>
2020-12-31 15:02:07 -0300

    Adiciona JPA repository forma pagamento

--- src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -1,16 +1,9 @@
 package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface FormaPagamentoRepository {
-
-    List<FormaPagamento> todas();
-
-    FormaPagamento porId(Long id);
-
-    FormaPagamento adicionar(FormaPagamento formaPagamento);
-
-    void remover(FormaPagamento formaPagamento);
+@Repository
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 }
