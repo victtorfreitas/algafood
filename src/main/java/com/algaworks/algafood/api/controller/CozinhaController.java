2f4d0775c321836f15e32a48b33971e03b0c14a2
Victtor <victtorfreitas95@gmail.com>
2020-12-31 11:08:31 -0300

    Altera segurança service

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -25,7 +25,7 @@ import java.util.List;
 public class CozinhaController {
 
     private final CozinhaRepository cozinhaRepository;
-    private CadastroCozinhaService cozinhaService;
+    private final CadastroCozinhaService cozinhaService;
 
     public CozinhaController(CozinhaRepository cozinhaRepository, CadastroCozinhaService cozinhaService) {
         this.cozinhaRepository = cozinhaRepository;
