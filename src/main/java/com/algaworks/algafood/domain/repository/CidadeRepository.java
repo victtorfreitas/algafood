539b0ac83e9f77dc5185903831f7d7f342757796
Victtor <victtorfreitas95@gmail.com>
2020-12-31 15:01:15 -0300

    Adiciona JPA repository cidade

--- src/main/java/com/algaworks/algafood/domain/repository/CidadeRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CidadeRepository.java
@@ -1,16 +1,9 @@
 package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface CidadeRepository {
-
-    List<Cidade> todas();
-
-    Cidade porId(Long id);
-
-    Cidade adicionar(Cidade cidade);
-
-    void remover(Long id);
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -30,12 +30,12 @@ public class CadastroCidadeService {
 
         cidade.setEstado(estado);
 
-        return cidadeRepository.adicionar(cidade);
+        return cidadeRepository.save(cidade);
     }
 
     public void excluir(Long cidadeId) {
         try {
-            cidadeRepository.remover(cidadeId);
+            cidadeRepository.deleteById(cidadeId);
 
         } catch (EmptyResultDataAccessException e) {
             throw new EntidadeNaoEncontradaException(
